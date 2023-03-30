36192e40aeee071927a07b0a68a330597de05882
Luke Karrys <luke@lukekarrys.com>
2023-03-30 13:34:00 -0700

    refactor

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -4,8 +4,10 @@ module.exports = {
   apps: [
     {
       name: 'sonos-nfc',
-      max_restarts: 1,
+      cwd: __dirname,
       combine_logs: true,
+      watch: true,
+      ignore_watch: ['node_modules'],
       script: './lib/index.js',
       interpreter: FNM ? `${FNM}/bin/node` : 'node',
     },
