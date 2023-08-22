eda9d7b8da70eb3db7c7a1a60b24f0594fad0360
Luke Karrys <luke@lukekarrys.com>
2023-08-22 09:36:01 -0700

    restart app

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
       cwd: __dirname,
       combine_logs: true,
       watch: true,
+      cron_restart: '0 6,18 * * *',
       ignore_watch: ['node_modules', '.git'],
       script: './lib/index.js',
       interpreter: FNM ? `${FNM}/aliases/18/bin/node` : 'node',
