06bc98f9232b76ef62978149913753281213ec77
Luke Karrys <luke@lukekarrys.com>
2025-01-22 00:40:09 -0700

    cleanup pm2 config

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,16 +1,11 @@
-const FNM = process.env.FNM_DIR
-
 module.exports = {
   apps: [
     {
       name: 'sonos-nfc',
       cwd: __dirname,
       combine_logs: true,
-      watch: true,
       cron_restart: '0 6,18 * * *',
-      ignore_watch: ['node_modules', '.git'],
       script: './lib/index.ts',
-      interpreter: FNM ? `${FNM}/aliases/18/bin/node` : 'node',
     },
   ],
 }
